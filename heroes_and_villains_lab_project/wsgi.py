3fe404adc3d89727f853db79b9c9f1f7b9639cbb
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-09-27 13:43:20 -0400

    Created Base Django Connections

---
+++ heroes_and_villains_lab_project/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for heroes_and_villains_lab_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "heroes_and_villains_lab_project.settings"
+)
+
+application = get_wsgi_application()
