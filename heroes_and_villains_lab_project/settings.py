1621351fc93d052c89ee3bb9b56ea019654872cf
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-09-27 18:07:06 -0400

    for instructor review: EOD created supers and super_types, unfinished migration

--- heroes_and_villains_lab_project/settings.py
+++ heroes_and_villains_lab_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%)ulso9e6y4z4(eggb(96mj%3vw@cz&d$@6ae@q$d!75b4bn6f"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +34,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'supers',
+    'super_types',
 ]
 
 MIDDLEWARE = [
@@ -68,17 +68,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "heroes_and_villains_lab_project.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
@@ -121,3 +110,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+try:
+    from heroes_and_villains_lab_project.local_settings import *
+except ImportError:
+    pass
